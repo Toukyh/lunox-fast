31ab771d1ffb242ff04267ebd52834eede91df90
Toky Yves <toky.r@nosytech.fr>
2024-08-08 11:18:57 +0300

    first commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -19,11 +19,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app.title).toEqual('lunox-fast');
   });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, lunox-fast');
-  });
 });
